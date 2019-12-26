a1958ece5ee1ad408ea446de6b7b978aed77236d
Micah Levine <micah.levine@outlook.com>
2019-12-26 18:41:49 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,8 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish "Hello, my name is/"
+    resp.write "Hello, my name is"
+    resp.finish
   end
-end 
 end
